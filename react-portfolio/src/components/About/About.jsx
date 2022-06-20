b4c0a779c217b79a65d6c8ffa3457826890d8ce8
KennyEe55 <99886058+KennyEe55@users.noreply.github.com>
2022-06-20 09:12:23 +0800

    Updates on the About sections

--- react-portfolio/src/components/About/About.jsx
+++ react-portfolio/src/components/About/About.jsx
@@ -1,8 +1,46 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/me-about.jpg'
+import {FaAward} from 'react-icons/fa'
+import {FaUserFriends} from 'react-icons/fa'
+import {FaFolder} from 'react-icons/fa'
 
 export default function About () {
   return (
-    <section id="about">About</section>
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-img">
+            <img src={ME} alt="About image" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+            <FaAward className='about__icon'/>
+            <h5>Experience</h5>
+            <small>1+ year</small>
+            </article>
+            <article className='about__card'>
+            <FaUserFriends className='about__icon'/>
+            <h5>Clients</h5>
+            <small>200+ clients</small>
+            </article>
+            <article className='about__card'>
+            <FaFolder className='about__icon'/>
+            <h5>Projects</h5>
+            <small>2 main projects</small>
+            </article>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto facilis quae deleniti dolore veritatis labore ut, ex molestias impedit consequatur, molestiae, assumenda error officiis commodi obcaecati ipsa quas. Hic, quibusdam!
+          </p>
+        </div>
+      </div>
+    </section>
   )
 }
